8dd715dd2bf8e9688bd018eb635dbd50db45d902
Aleuetdinov <altynaleuetdinov@gmail.com>
2025-05-13 09:51:30 +0500

    Feat:title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,9 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Vue Laravel App</title>
+    <title>Qoldau</title>
+    <link rel="icon" type="image/png" href="/storage/navbarImage/logo.png" />
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="preload" as="image" href="/storage/app/public/navbarImage/navbarPhoto1.jpg">
     <link rel="preload" as="image" href="/storage/app/public/navbarImage/navbarPhoto2.jpg">
